73290c1e7e8abb92834d54d3495d6e0753a61582
mipengli <lipengguanghui@163.com>
2023-12-26 17:16:42 +0800

    hot-fix commit1

--- src/main/java/com/itheima/web/AddServlet.java
+++ src/main/java/com/itheima/web/AddServlet.java
@@ -17,8 +17,8 @@ public class AddServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println();
-        System.out.println();
+        System.out.println("666666666666666666666666666666");
+
         //1. 接收数据,request.getParameter 不能接收json的数据
        /* String brandName = request.getParameter("brandName");
         System.out.println(brandName);*/
